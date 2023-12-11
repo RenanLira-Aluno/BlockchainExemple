c9755e2b3923b2bdbcec1a9d3e2130a6be8339a2
Renan-Lira <renan.almeida@lojaspintos.com.br>
2023-12-11 11:35:02 -0300

    testes

--- test.js
+++ test.js
@@ -2,8 +2,9 @@ import { Blockchain } from "./blockchain.js";
 
 const bitcoin = new Blockchain()
 
-bitcoin.createTransaction(100, "Alice", "Bob")
-bitcoin.createTransaction(50, "Bob", "Alice")
+for (let i = 0; i < 10000; i++) {
+  bitcoin.createTransaction(100, "address1", "address2")
+}
 
 bitcoin.createNewBlock()
 
